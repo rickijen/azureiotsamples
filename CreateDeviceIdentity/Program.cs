a590401af984ad595c69d1152eadef863043904b
Rick Jen <rijen@microsoft.com>
2018-03-02 14:25:16 -0800

    Add project files.

---
+++ CreateDeviceIdentity/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace CreateDeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=rmtmonitor6d38c.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=mvdtgRmtWv5SmHGZoXjL/U4w9cNKZZ8389XeGhcXLd8=";
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "CoolSculptingDevice-002";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimulatedDevice/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static string iotHubUri = "rmtmonitor6d38c.azure-devices.net";
+        static string deviceKey = "A1vDudLHLfLQfmZ6s7RyCfNEMuyGDEENxEIfTW1To+I=";
+
+        private static async void ReceiveC2dAsync()
+        {
+            Console.WriteLine("\nReceiving cloud to device messages from service");
+            while (true)
+            {
+                Message receivedMessage = await deviceClient.ReceiveAsync();
+                if (receivedMessage == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received message: {0}", Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+                Console.ResetColor();
+
+                await deviceClient.CompleteAsync(receivedMessage);
+            }
+        }
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double minTemperature = 20;
+            double minHumidity = 60;
+            int messageId = 1;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                var telemetryDataPoint = new
+                {
+                    messageId = messageId++,
+                    deviceId = "CoolSculptingDevice-000",
+                    temperature = currentTemperature,
+                    humidity = currentHumidity
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                message.Properties.Add("XXXXXXXX temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                await Task.Delay(1000);
+            }
+        }
+
+        private static async void SendToBlobAsync()
+        {
+            string fileName = "image.jpg";
+            Console.WriteLine("Uploading file: {0}", fileName);
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            using (var sourceData = new FileStream(@"image.jpg", FileMode.Open))
+            {
+                await deviceClient.UploadToBlobAsync(fileName, sourceData);
+            }
+
+            watch.Stop();
+            Console.WriteLine("Time to upload file: {0}ms\n", watch.ElapsedMilliseconds);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("CoolSculptingDevice-000", deviceKey), TransportType.Mqtt);
+            deviceClient.ProductInfo = "XXXXXXXXX HappyPath_Simulated-CSharp";
+            SendDeviceToCloudMessagesAsync();
+            ReceiveC2dAsync();
+            //SendToBlobAsync();
+            Console.ReadLine();
+        }
+    }
+}
