9938cee96ec6e5c0878cd9db9fd2c2f118aa7b3c
Rick Jen <rickijen@gmail.com>
2018-03-06 14:03:35 -0800

    move connectionstring and device id to commandline args

--- AddTagsAndQuery/Program.cs
+++ AddTagsAndQuery/Program.cs
@@ -10,34 +10,44 @@ namespace AddTagsAndQuery
     class Program
     {
         static RegistryManager registryManager;
-        static string connectionString = "HostName=rmtmonitor6d38c.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=mvdtgRmtWv5SmHGZoXjL/U4w9cNKZZ8389XeGhcXLd8=";
-        public static async Task AddTagsAndQuery()
+        private static string connectionString;
+        private static string deviceId;
+        //static string connectionString = "HostName=rmtmonitor6d38c.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=mvdtgRmtWv5SmHGZoXjL/U4w9cNKZZ8389XeGhcXLd8=";
+        public static async Task AddTagsAndQuery(string deviceId)
         {
-            var twin = await registryManager.GetTwinAsync("CoolSculptingDevice-001");
+            var twin = await registryManager.GetTwinAsync(deviceId);
             var patch =
                 @"{
              tags: {
                  location: {
-                     region: 'US',
-                     plant: 'Redmond43'
+                     region: 'JP',
+                     clinic: 'Slim Tokyo'
                  }
              }
          }";
             await registryManager.UpdateTwinAsync(twin.DeviceId, patch, twin.ETag);
 
-            var query = registryManager.CreateQuery("SELECT * FROM devices WHERE tags.location.plant = 'Redmond43'", 100);
+            var query = registryManager.CreateQuery("SELECT * FROM devices WHERE tags.location.clinic = 'Slim Tokyo'", 100);
             var twinsInRedmond43 = await query.GetNextAsTwinAsync();
-            Console.WriteLine("Devices in Redmond43: {0}", string.Join(", ", twinsInRedmond43.Select(t => t.DeviceId)));
+            Console.WriteLine("Devices in Slim Tokyo: {0}", string.Join(", ", twinsInRedmond43.Select(t => t.DeviceId)));
 
-            query = registryManager.CreateQuery("SELECT * FROM devices WHERE tags.location.plant = 'Redmond43' AND properties.reported.connectivity.type = 'cellular'", 100);
+            query = registryManager.CreateQuery("SELECT * FROM devices WHERE tags.location.clinic = 'Slim Tokyo' AND properties.reported.connectivity.type = 'cellular'", 100);
             var twinsInRedmond43UsingCellular = await query.GetNextAsTwinAsync();
-            Console.WriteLine("Devices in Redmond43 using cellular network: {0}", string.Join(", ", twinsInRedmond43UsingCellular.Select(t => t.DeviceId)));
+            Console.WriteLine("Devices in Slim Tokyo using cellular network: {0}", string.Join(", ", twinsInRedmond43UsingCellular.Select(t => t.DeviceId)));
         }
 
         static void Main(string[] args)
         {
+            if (string.IsNullOrWhiteSpace(connectionString) && (args.Length < 2))
+            {
+                Console.WriteLine("AddTagsAndQuery <coonnection string> <device id>");
+                return;
+            }
+
+            connectionString = args[0];
+            deviceId = args[1];
             registryManager = RegistryManager.CreateFromConnectionString(connectionString);
-            AddTagsAndQuery().Wait();
+            AddTagsAndQuery(deviceId).Wait();
             Console.WriteLine("Press Enter to exit.");
             Console.ReadLine();
         }
